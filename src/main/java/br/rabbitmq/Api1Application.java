8d11418b37ebc63ac495000bca0572ec77cb8a95
Gabriel Bugliani Silva <gbuglian@everis.com>
2020-11-18 02:00:55 -0300

    My Firt Commit utilizing messaging

---
+++ src/main/java/br/rabbitmq/Api1Application.java
@@ -0,0 +1,15 @@
+package br.rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Api1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Api1Application.class, args);
+	}
+	
+	
+
+}
